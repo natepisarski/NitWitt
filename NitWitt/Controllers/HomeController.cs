7297fd73ab1c206e61ce4121a027f1bb2b1c746d
Nathaniel Pisarski <nathanpisarski@gmail.com>
2017-08-24 22:18:23 -0400

    Initial Commit - Create barebones WebAPI endpoint

---
+++ NitWitt/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace NitWitt.Controllers
+{
+    public class ViewController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult View(string twitterHandle) {
+            return Ok($"WebAPI works! {twitterHandle}");
+        }
+    }
+}
---
+++ NitWitt/Models/ViewRequest.cs
@@ -0,0 +1,20 @@
+﻿namespace NitWitt.Models
+{
+    namespace Nitwitt.API.Models
+    {
+        public enum ViewDetail
+        {
+            Light,
+            Medium,
+            Heavy
+        }
+
+        public class ViewRequest
+        {
+
+            public string TwitterHandle { get; set; }
+
+            public ViewDetail View { get; set; }
+        }
+    }
+}
